3cf52c33d12c015a2b998f9ea6a13c743c8d038e
Александр Гаганов <althgamer@althgamer-macbook.local>
2024-04-25 14:02:23 +0300

    LU decomposition method for SLE solving added

---
+++ LUDecompositionMethod.cs
@@ -0,0 +1,55 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        const int n = 3;
+        double[,] A = new double[n, n] {{1, 2, -1},
+                                        {2, -3, 2},
+                                        {3, 1, 1}};
+        double[] b = [2, 2, 8];
+        Console.WriteLine(string.Join(" ", LUDecomposition(A, b, n)));
+    }
+
+    public static double[] LUDecomposition(double[,] matrix, double[] rightPart, int n)
+    {
+        double[,] lu = new double[n, n];
+        double sum;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                sum = 0;
+                for (int k = 0; k < i; k++)
+                    sum += lu[i, k] * lu[k, j];
+                lu[i, j] = matrix[i, j] - sum;
+            }
+            for (int j = i + 1; j < n; j++)
+            {
+                sum = 0;
+                for (int k = 0; k < i; k++)
+                    sum += lu[j, k] * lu[k, i];
+                lu[j, i] = (matrix[j, i] - sum) / lu[i, i];
+            }
+        }
+
+        double[] y = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            sum = 0;
+            for (int k = 0; k < i; k++)
+                sum += lu[i, k] * y[k];
+            y[i] = rightPart[i] - sum;
+        }
+
+        double[] x = new double[n];
+        for (int i = n - 1; i >= 0; i--)
+        {
+            sum = 0;
+            for (int k = i + 1; k < n; k++)
+                sum += lu[i, k] * x[k];
+            x[i] = (y[i] - sum) / lu[i, i];
+        }
+        
+        return x;
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 - \# 3(of 27.02.24): [theorems.py](https://github.com/GaganovAlexander/numerical_methods/blob/main/dichotomy.py) roots search limits theorems 
 - \# 4(of 05.03.24): [chord.py](https://github.com/GaganovAlexander/numerical_methods/blob/main/chord.py) chord method
 - \# 5(of 12.03.24): [sle.py](https://github.com/GaganovAlexander/numerical_methods/blob/main/sle.py) single division, rectangle and max of column methods
+- \# 6(of 24.04.24) [LUDecompositionMethod](https://github.com/GaganovAlexander/numerical_methods/blob/main/LUDecompositionMethod.cs) LU decomposition method for SLE solving
+
+    .exe file download:
+    - [LUDecompositionMethod](https://althgamer.ru/storage/LUDecompositionMethod.exe)
+
 - \# 7(of 09.04.24): [PVIterMethod.cs](https://github.com/GaganovAlexander/numerical_methods/blob/main/PVIterMethod.cs)/[PVDirectUnwindingMethod.cs](https://github.com/GaganovAlexander/numerical_methods/blob/main/PVDirectUnwindingMethod.cs) iteration/direct unwinding method for proper value finding
 
     .exe files download:
